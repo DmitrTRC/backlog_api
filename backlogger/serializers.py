06bf8326ba887d0254648c26bcf3ac1e799fda9f
Dmitry Morozov <9984398@gmail.com>
2023-11-02 11:34:54 +0300

    -> backlogitem: Serializer: Done

---
+++ backlogger/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from models.mBackLogItem import BackLogItem
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email']
+
+
+class BackLogItemSerializer(serializers.ModelSerializer):
+    created_by = UserSerializer(read_only=True)
+
+    class Meta:
+        model = BackLogItem
+        fields = [
+            'id', 'title', 'description', 'priority', 'stage',
+            'created_at', 'updated_at', 'created_by'
+        ]
